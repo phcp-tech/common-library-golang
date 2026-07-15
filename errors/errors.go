e1fb356cf9fa54ba08628c3be5cfa89f01649777
Gordon <rts.gordon@gmail.com>
2026-07-15 20:20:12 +1000

    rename errorcode to errors

--- errors/errors.go
+++ errors/errors.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package errorcode
+package errors
 
 import "github.com/phcp-tech/common-library-golang/dto"
 
